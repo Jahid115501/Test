65ecfe91b059b8725d660e29e615a24f1ab8af67
Jahid115501 <hasan23105101002@diu.edu.bd>
2024-11-02 19:04:08 +0600

    Initial commit

---
+++ src/main/java/com/mycompany/test2/Test2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test2;
+
+/**
+ *
+ * @author U S E R
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
